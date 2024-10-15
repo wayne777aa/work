95c98d364fddb5cfefa0726387ddee8ac781cd0a
wayne777aa <wayne777aa@gmail.com>
2024-10-15 22:31:31 +0800

    lab2PE

--- IntroAlgorithms/lab2/pE.cpp
+++ IntroAlgorithms/lab2/pE.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
-const int Mod = 998244353;
+const long long Mod = 998244353;
 
-long long arr[500000];
-long long sorted[500000];
+long long arr[1000000];
+long long sorted[1000000];
 
-long long merge_sort(long long *arr,int l, int r) {
+long long merge_sort(long long *arr,long long l, long long r) {
     if (l >= r) return 0;
-    int mid = (l + r)/2;
+    long long mid = (l + r)/2;
     long long cnt = (merge_sort(arr,l, mid) + merge_sort(arr,mid + 1, r))% Mod;
-    int k = 0, i = l, j = mid + 1;
+    long long k = 0, i = l, j = mid + 1;
     while (i <= mid || j <= r){
         if(i <= mid && j <= r){
             if (arr[i] <= arr[j]){
@@ -42,50 +42,34 @@ int main(){
     }
     long long cnt1=0;//循環一次出來的cnt
     long long cnt2=0;//循環完剩的數量去cnt
-    long long over = K%N;
+    long long over = (K%N)%Mod;
+    copy(input, input+N, arr);
+    long long num = merge_sort(arr,0,N-1)%Mod;
+    copy(input, input+N, arr);
+    sort(arr,arr+N,cmp);
+    map<long long,long long> bigger,smaller;
+    for(int i=0;i<N;i++){
+        bigger[arr[i]]=N-1-i;
+    }
+    for(int i=N-1;i>=0;i--){
+        smaller[arr[i]] = i;
+    }
     if(K>=N){
-        copy(input, input+N, arr);
-        long long num = merge_sort(arr,0,N-1);
-        map<long long,long long> rank,repeat;
-        long long ranknum = 0;
-        for(int i=0;i<N;i++){
-            if(arr[i] > arr[i-1]){
-                rank[arr[i]] = ranknum;
-            }
-            if(arr[i] == arr[i-1]){
-                rank[arr[i]] = ranknum;
-                repeat[arr[i]]++;
-            }
-            ranknum++; //越大的排名越後面
-        }
-        cnt1 = num;
+        cnt1 = num%Mod;
         if(over>0)
-        cnt2 = num;
+        cnt2 = num%Mod;
         for(int i=0;i<N-1;i++){ //做到N-2(N-1排到第一位)
-            num = (num + (N - rank[input[i]] - 1) - (rank[input[i]]) + repeat[input[i]])% Mod; //(N-rank-1) - (rank)+(repeat)
+            num = (num + bigger[input[i]] - smaller[input[i]])% Mod; //(bigger) - (smaller)
                 cnt1 = (cnt1 + num)%Mod;
             if(i<over-1){
                 cnt2 = (cnt2 + num)%Mod;
             }
         }
     }else{
-        copy(input, input+N, arr);
-        long long num = merge_sort(arr,0,N-1);
-        map<long long,long long> rank,repeat;
-        long long ranknum = 0;
-        for(int i=0;i<N;i++){
-            if(arr[i] > arr[i-1])
-                rank[arr[i]] = ranknum;
-            if(arr[i] == arr[i-1]){
-                rank[arr[i]] = ranknum;
-                repeat[arr[i]]++;
-            }
-            ranknum++; //越大的排名越後面
-        }
         if(over>0)
         cnt2 = num;
         for(int i=0;i<over-1;i++){ //做到N-2(N-1排到第一位)
-            num = num + (N - rank[input[i]] - 1) - (rank[input[i]]) + repeat[input[i]]; //(N-rank-1) - (rank) +(repeat)
+            num = (num + bigger[input[i]] - smaller[input[i]])% Mod; //(N-rank-1) - (rank) +(repeat)
                 cnt2 = (cnt2 + num)%Mod;
         }
     }
@@ -94,12 +78,12 @@ int main(){
     cnt1 = (cnt1*route)%Mod;
     
 //-------------不同循環間的數量-------------
-    long long cnt3;
+    long long cnt3 =0;
     sort(input,input+N,cmp);
     sort(input+N,input+2*N,cmp);
-    cnt3 = merge_sort(input,0,2*N-1);
-    long long temp = (K-1)%Mod;
-    temp = ((temp*(temp+1))/2);
+    cnt3 = merge_sort(input,0,2*N-1)%Mod;
+    long long temp = (K)%Mod;
+    temp = ((temp*(temp-1))/2);
     temp %= Mod;
     cnt3 = (cnt3*temp)%Mod;
     long long result = ((cnt1+cnt2)%Mod+cnt3)%Mod;//1+2+3+...+K-1
