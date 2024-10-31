452786bf753ecbe9410da6b98fec5cc21ec1ef12
wayne777aa <wayne777aa@gmail.com>
2024-10-31 20:44:28 +0800

    lab3pB

--- IntroAlgorithms/lab3/pB.cpp
+++ IntroAlgorithms/lab3/pB.cpp
@@ -10,8 +10,9 @@ struct Node{
 Node* root=0;
 int preorder[200001],inorder[200001];
 int N;
-// map<int, int> inorderindex;
-int inorderindex[200001];
+map<int, int> inorderindex;
+// int inorderindex[200001];
+bool flag=false;
 
 
 
@@ -21,9 +22,13 @@ bool cmp(int a,int b){
 
 Node* buildtree(int root_i,int l, int r){ //preorder_root_index, inorder_left, inorder_right
     if(l > r) return nullptr;
+    if(root_i>=N || flag == true){          //invalid preorder
+        flag = true;
+        return nullptr;
+    }
     Node* node = new Node;
-    int size_left = inorderindex[root_i];               //array
-    // int size_left = inorderindex[preorder[root_i]];  //map
+    // int size_left = inorderindex[root_i];         //array
+    int size_left = inorderindex[preorder[root_i]];  //map
     node -> val = preorder[root_i];
     node -> l = buildtree(root_i+1,l,size_left-1);
     node -> r = buildtree(root_i+size_left-l+1,size_left+1,r);
@@ -61,14 +66,17 @@ int main(){
     }
     sort(inorder,inorder+N,cmp);
     
-    for(int i = 0; i<N; i++){       //array
-        inorderindex[i] = BinarySearch(preorder[i]);
-    }
-
-    // for(int i = 0; i<N; i++){    //map
-    //    inorderindex[inorder[i]] = i;
+    // for(int i = 0; i<N; i++){       //array
+        // inorderindex[i] = BinarySearch(preorder[i]);
     // }
+
+    for(int i = 0; i<N; i++){    //map
+       inorderindex[inorder[i]] = i;
+    }
     root = buildtree(0,0,N-1);
-    Postorder(root);
+    if(flag == false)
+        Postorder(root);
+    else
+        cout << -1;
     return 0;
 }
