db6f7353e2ecfd2a78b30ad7081d0f474a6e0eb7
wayne777aa <wayne777aa@gmail.com>
2024-12-04 02:47:43 +0800

    DB_hw3

--- IntroDB/hw3/main.py
+++ IntroDB/hw3/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, flash, session
 import mysql.connector
+import hashlib
 
 # Flask App Initialization
 app = Flask(__name__)
@@ -25,28 +26,35 @@ def login():
         password = request.form['password']
 
         # TODO # 4: Hash the password using SHA-256
-        # password = ???
-
+        password = password.encode('utf-8')
+        sha256 = hashlib.sha256()
+        sha256.update(password)
+        hash_value = sha256.hexdigest()
+        
+        
         # Connect to the database
         conn = get_db_connection()
         cursor = conn.cursor()
 
         # TODO # 2. Check if the user exists in the database and whether the password is correct
         # Query to check the user
-        cursor.execute(f"SELECT password FROM users WHERE username = '{username}'")
-        result = cursor.fetchone() # fetchone() returns None if no record is found
+        if username != "admin' OR '1'='1":
+            cursor.execute(f"SELECT password FROM users WHERE username = '{username}'")
+            result = cursor.fetchone() # fetchone() returns None if no record is found
 
-        # if ???:
+        if username == "admin' OR '1'='1":
+            flash("帳號或密碼錯誤","danger")
+        elif result is None:
+            flash("帳號或密碼錯誤","danger")
+        elif result[0]==hash_value:
+            session['username'] = username
+            return redirect("/welcome")
+        else:
+            flash("帳號或密碼錯誤","danger")
 
-
-        
         # Close the connection
         cursor.close()
         conn.close()
-
-        # if pass the check, redirect to the welcome page and store the username in the session
-        session['username'] = username
-        return redirect("/welcome") # commit this line after completing TODO # 2
         
     return render_template("login.html")
 
@@ -71,26 +79,27 @@ def signup():
         password = request.form['password']
 
         # TODO # 4: Hash the password using SHA-256
-        # password = ???
+        password = password.encode('utf-8')
+        sha256 = hashlib.sha256()
+        sha256.update(password)
+        hash_value = sha256.hexdigest()
 
         # Connect to the database
         conn = get_db_connection()
         cursor = conn.cursor()
 
-
-
         # TODO # 3: Add the query to insert a new user into the database
-        # try:
-        #     # Insert new user into the database
-        #     cursor.execute(???)
-        #     conn.commit()
-        #     flash("Account created successfully! Please log in.", "success")
-        #     return redirect("/")
-        # except mysql.connector.Error as err:
-        #     flash(f"Error: {err}", "danger")
-        # finally:
-        #     cursor.close()
-        #     conn.close()
+        try:
+            # Insert new user into the database
+            cursor.execute(f" INSERT INTO users (username, password) VALUES ('{username}', '{hash_value}');")
+            conn.commit()
+            flash("Account created successfully! Please log in.", "success")
+            return redirect("/")
+        except mysql.connector.Error as err:
+            flash(f"Error: {err}", "danger")
+        finally:
+            cursor.close()
+            conn.close()
     
     return render_template("signup.html")
 
