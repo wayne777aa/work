bc91120cfb8d14c8a749965201e7bc2faa520b93
wayne777aa <wayne777aa@gmail.com>
2025-03-15 00:43:52 +0800

    lab1

---
+++ ComputerOrganization/gcd_lcm.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int gcd(int a, int b) {
+    int i=a;
+    if(a>b){
+        i=b;
+    }
+    while(i>0){
+        if(a%i == 0 && b%i == 0){
+            return i;
+        }
+        i--;
+    }
+    return 1;
+}
+
+int lcm(int a, int b) {
+    int temp = gcd(a,b);
+    return (a*b)/temp;
+}
+
+int main() {
+    // DO NOT modify this section
+    int n1, n2;
+    printf("Please enter the first number: ");
+    scanf("%d", &n1);
+    printf("Please enter the second number: ");
+    scanf("%d", &n2);
+
+    printf("%d %d\n", gcd(n1, n2), lcm(n1, n2));
+
+    return 0;
+}
---
+++ ComputerOrganization/sum_of_digits.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int sumOfDigits(int n) {
+    // TODO
+}
+
+int main() {
+    // DO NOT modify this section
+    int num;
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    printf("%d\n", sumOfDigits(num));
+
+    return 0;
+}
